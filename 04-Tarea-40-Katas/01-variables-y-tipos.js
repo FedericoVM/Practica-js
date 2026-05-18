0c2fc8f27e2b94e0b1e07bca41fa8f23d73ca39d
Federico Valdiglesias <federicomrtl@gmail.com>
2026-05-18 20:46:30 -0300

    feat: Finalizado la ejercitacion de variables y tipos

--- 04-Tarea-40-Katas/01-variables-y-tipos.js
+++ 04-Tarea-40-Katas/01-variables-y-tipos.js
@@ -10,6 +10,7 @@
 -------------------------------------------------------------------------- */
 function sumar(a, b) {
   // TU CÓDIGO AQUÍ 👇
+  return a + b;
 }
 
 /* --------------------------------------------------------------------------
@@ -18,6 +19,7 @@ function sumar(a, b) {
 -------------------------------------------------------------------------- */
 function restar(a, b) {
   // TU CÓDIGO AQUÍ 👇
+  return a - b ;
 }
 
 /* --------------------------------------------------------------------------
@@ -27,6 +29,7 @@ function restar(a, b) {
 -------------------------------------------------------------------------- */
 function unirNombre(nombre, apellido) {
   // TU CÓDIGO AQUÍ 👇
+  return `${nombre} ${apellido}`;
 }
 
 /* --------------------------------------------------------------------------
@@ -36,6 +39,7 @@ function unirNombre(nombre, apellido) {
 -------------------------------------------------------------------------- */
 function crearSaludo(nombre, edad) {
   // TU CÓDIGO AQUÍ 👇
+  return `Hola, soy ${nombre} y tengo ${edad} años.`
 }
 
 /* --------------------------------------------------------------------------
@@ -44,6 +48,7 @@ function crearSaludo(nombre, edad) {
 -------------------------------------------------------------------------- */
 function detectarTipo(valor) {
   // TU CÓDIGO AQUÍ 👇
+  return typeof valor;
 }
 
 /* --------------------------------------------------------------------------
@@ -53,6 +58,8 @@ function detectarTipo(valor) {
 -------------------------------------------------------------------------- */
 function convertirANumero(str) {
   // TU CÓDIGO AQUÍ 👇
+  let convertirANum = Number(str);
+  return convertirANum; 
 }
 
 /* --------------------------------------------------------------------------
@@ -61,6 +68,7 @@ function convertirANumero(str) {
 -------------------------------------------------------------------------- */
 function calcularLongitud(str) {
   // TU CÓDIGO AQUÍ 👇
+  return str.length;
 }
 
 /* --------------------------------------------------------------------------
@@ -69,6 +77,7 @@ function calcularLongitud(str) {
 -------------------------------------------------------------------------- */
 function aMayusculas(str) {
   // TU CÓDIGO AQUÍ 👇
+  return str.toUpperCase();
 }
 
 /* --------------------------------------------------------------------------
@@ -79,6 +88,8 @@ function aMayusculas(str) {
 -------------------------------------------------------------------------- */
 function calcularAreaCirculo(radio) {
   // TU CÓDIGO AQUÍ 👇
+  let resultado = Math.PI * radio * radio;
+  return Math.round(resultado * 100) / 100;
 }
 
 /* --------------------------------------------------------------------------
@@ -88,6 +99,11 @@ function calcularAreaCirculo(radio) {
 -------------------------------------------------------------------------- */
 function verificarNaN(valor) {
   // TU CÓDIGO AQUÍ 👇
+  if (isNaN(valor)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // 🚨 ¡NO TOCAR ESTA LÍNEA! Necesaria para los tests.
